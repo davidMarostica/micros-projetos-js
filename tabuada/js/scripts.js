5f7e713f9a1a618aea4af8e4c53ed53ea7b06062
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-11-20 09:17:46 -0300

    ciranda taplade

--- tabuada/js/scripts.js
+++ tabuada/js/scripts.js
@@ -1,1 +1,40 @@
-console.log("teste");
+// Seleções de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const operationsContainer = document.querySelector(
+  "#multiplication-operations"
+);
+const multiplicationTable = document.querySelector("#multiplicator-operations");
+
+// Função para criar a tabuada
+const createTable = (number, multiplicatorNumber) => {
+  operationsContainer.innerHTML = "";
+
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
+
+    const templete = `<div class="row> 
+    <div class="operation">${number} x ${i} =</div>
+    <div class="result">${result}</div> 
+    </div>`;
+  }
+};
+// Evento de envio do formulário
+multiplicationForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const multiplicationNumber = +numberInput.value;
+  const multiplicatorNumber = +multiplicationInput.value;
+
+  console.log("Número base:", multiplicationNumber);
+  console.log("Multiplicar até:", multiplicatorNumber);
+
+  if (!multiplicationNumber || !multiplicatorNumber) {
+    console.log("Por favor, preencha os dois campos com números válidos.");
+    return;
+  }
+
+  titleSpan.textContent = multiplicationNumber;
+  createTable(multiplicationNumber, multiplicatorNumber);
+});
